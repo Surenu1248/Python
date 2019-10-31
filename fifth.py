b13905e4d6bba1b915633295d01e9a9a449ffde5
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-10-31 15:20:37 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -1,7 +1,7 @@
 # 5.Given a list of numbers, return a list where all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or modify the passed in list.
 
-s = [1, 2, 2, 3]
+s = [4,5,6,7,8]
 b = []
 
 
