2088be7d6a785b607948e38739df81c5eb8088fe
Surendra Katla <31179719+Surenu1248@users.noreply.github.com>
2019-10-31 15:19:01 +0530

    Update second.py

--- second.py
+++ second.py
@@ -1,7 +1,7 @@
 # 2.Given a list of strings, return the count of the number of strings ,
 # where the string length is 2 or more and the first and last chars of the string are the same.
 
-a = ['axa', 'xyz', 'gg', 'x', 'yyy']
+a = ['bba','jjk','caba','ghjk']
 c = []
 
 
